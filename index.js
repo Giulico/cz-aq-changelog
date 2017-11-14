fd601ffa5176463c9db68393520eee6973c1d699
giulio collesei <giulio.collesei+gitlab@aquest.it>
2017-11-14 10:35:39 +0100

    refactor(Types): Clean types

--- index.js
+++ index.js
@@ -3,6 +3,16 @@
 var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types');
 
+delete conventionalCommitTypes.types.ci; // Useless
+delete conventionalCommitTypes.types.chore; // Useless
+delete conventionalCommitTypes.types.test; // Use feature
+delete conventionalCommitTypes.types.perf; // Use refactor
+delete conventionalCommitTypes.types.fix; // Use refactor
+
+conventionalCommitTypes.types.refactor.description = "Changes to fix bugs, improve performance and tests";
+conventionalCommitTypes.types.style.description = "Changes that affect the style of your appication (scss, formatting, ecc)";
+conventionalCommitTypes.types.build.description = "Changes that affect the build system or external dependencies (Webpack, npm, git, ecc)";
+
 module.exports = engine({
   types: conventionalCommitTypes.types
 });
